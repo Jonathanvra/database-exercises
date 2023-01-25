e13f00023e203b106149d631786677168aae0dab
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-01-25 15:24:09 -0600

    finished where exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,19 @@
 USE codeup_test_db;
-    SHOW TABLES;
 
+-- The name of all albums by Pink Floyd
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+-- The year Sgt. Pepper's Lonely Hearts Club Band was released.
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+-- The genre of Nevermind.
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+-- Albums released in the 1990's
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+-- Which albums had less than 20 million sales.
+SELECT name FROM albums WHERE sales < 20;
+
+-- All the albums with the genre of rock.
+SELECT  name FROM albums WHERE genre = 'Rock';

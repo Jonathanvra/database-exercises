82a0c9180124094b05a5ed779a84abddb86491a1
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2023-01-31 15:03:21 -0600

    pausing to do datebase design

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,13 +2,14 @@ USE employees;
 
 # Find all the employees with the same hire date as employee 101010 using a subquery.
 # 69 Rows
-SELECT e.first_name, e.last_name
-FROM employees as e
-WHERE emp_no IN (SELECT emp_no
-                 FROM dept_manager
-                 WHERE to_date = '9999-01-01');
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, e.hire_date
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees AS e
-WHERE emp_no IN (SELECT emp_no
+WHERE e.hire_date IN (SELECT hire_date
                  FROM employees
-                 WHERE hire_date = '1999-10-22');
+                 WHERE emp_no = 101010);
+
+# Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+
+
+# Find all the current department managers that are female.
